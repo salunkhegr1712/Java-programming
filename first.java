d4baf7ce55f995655ccb00a7c3d1e572c07bee0c
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-08-29 21:42:26 +0530

    print,println,skelton of java and installed jdk for java

---
+++ first.java
@@ -0,0 +1,16 @@
+
+// This is a simple Java program.
+// FileName : "first.java".
+ 
+// it is very good habbit to name the class same name that of the file 
+class first
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    // the public static void main() it is main function inside the class in the java 
+    public static void main(String args[])
+    {
+        // System.out.print(obj); this  is the syntax to display output on the screen with java 
+        System.out.print("Hello World Ghansham here");
+    }
+}
---
+++ helloGhansham.java
@@ -0,0 +1,20 @@
+
+public class helloGhansham {
+    public static void main(String[] args) {
+        // system.out is basically focusing on the output of the file 
+        System.out.print("hello ghansham salunkhe here");
+        // we can pass a object or a string inside the system.out.print function 
+    }
+    
+}
+
+// you can able to run the code by compiling first and then running the class which you
+// get after compiling with the java ex:
+
+// to compile the file 
+// javac filename.java 
+
+// after compiling youwill get a file of type object and it is a executable file you can able to run it with 
+
+// java filename.class
+// even that you dont have to be use the extension .class in that matter 
