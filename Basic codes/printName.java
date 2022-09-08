8fa9d8e7cf060243af2bdd202cd35026449a95f7
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-09-09 00:06:57 +0530

    added some regex programmes

--- Basic codes/printName.java
+++ Basic codes/printName.java
@@ -4,22 +4,11 @@ public class printName {
     public static void main(String[] args) {
 
         Scanner input=new Scanner(System.in);
-        // declaring variable 
-        int c;
-        float f;
         String g,gg;
 
 
-        c=input.nextInt();
-        // if you provide float input to integer input it will not show any error 
-            //  7.8
-            // Exception in thread "main" java.util.InputMismatchException
-            //         at java.base/java.util.Scanner.throwFor(Scanner.java:939)
-            //         at java.base/java.util.Scanner.next(Scanner.java:1594)
-            //         at java.base/java.util.Scanner.nextInt(Scanner.java:2258)
-            //         at java.base/java.util.Scanner.nextInt(Scanner.java:2212)
-            //         at printName.main(printName.java:13)
-        f=input.nextFloat();
+        input.nextInt();
+        input.nextFloat();
 
 
         g=input.next();
