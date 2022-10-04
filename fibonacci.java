896bdbf7cbabcede74ac106a978cb35da9674ea0
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-10-04 13:56:15 +0530

    codes for problems on loops

--- fibonacci.java
+++ fibonacci.java
@@ -1,5 +1,23 @@
 import java.lang.*;
 
+// following code will print the fibonacci series for the number of value you give using loop 
 public class fibonacci {
-    public static void main()
+    public static void main(String args[]){
+        int n=10;
+        System.out.println("the fibonacci series is : ");
+        int k=0;int l=1,g;
+        for(int i=0;i<n;i++){
+            if(i==0){
+                System.out.println(k);
+            }
+            else {
+                g=k;
+                k=k+l;
+                l=g;
+                System.out.println(k);
+
+            }
+
+        }
+    }
 }
