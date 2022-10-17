3dba3c69b6ed52be0da54fe53af462dadc286607
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-10-17 22:00:22 +0530

    added some files on the java

---
+++ methods.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class methods {
+
+    // so it compulsion that we only have to write function inside the class and we must have to follow it 
+    static void print(String s){
+        System.out.print(s);
+    } 
+
+    static void println(String s){
+        System.out.println(s);
+    }
+
+    public static void main(String args[]){
+        
+
+    }
+}
---
+++ newFile.java
@@ -0,0 +1,24 @@
+// importing basic functionalities from the java library
+import java.lang.*;
+
+// this is sample file which i created so that i can test my java boilerplate inside this file nothing else 
+
+// the main class
+public class newFile {
+
+    // basic code so that i can save my time as im so lazy to write System.out.print()
+    static void print(String s){
+
+        System.out.print(s);
+    }
+
+    static void println(String s){
+
+        System.out.println(s);
+    }
+
+    // main function of our java code
+    public static void main(String args[]){
+
+    }
+}
