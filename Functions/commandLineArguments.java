684754da0493a76cc57f8446ee80199552ae5434
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-10-22 16:31:36 +0530

    added some infoemation about the functions inside the java programming laguage

---
+++ Functions/commandLineArguments.java
@@ -0,0 +1,29 @@
+// importing basic functionalities from the java library
+import java.lang.*;
+import java.util.*;
+
+// the main class
+public class commandLineArguments{
+ 
+    // basic code so that i can save my time as im so lazy to write System.out.print()
+    static void print(String s){
+
+        System.out.print(s);
+    }
+
+    static void println(String s){
+
+        System.out.println(s);
+    }
+
+    // so we will see how the command line argumnets will work in java programming language
+
+    // main function of our java code
+
+    // the string args is iteself  is one of the command line argumnets tool inside the java programming language
+    public static void main(String args[]){
+
+        so lets print the command that we get from the terminal while running the object code and we will also going to use the
+        command line tools in order to use them inside the java programming and it has so many applic
+    }
+}
