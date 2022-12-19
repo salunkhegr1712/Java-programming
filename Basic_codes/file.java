1615316ee3304287cafcb611aa5ed516afe1cd15
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-12-19 11:16:25 +0530

    revision on string and operators

--- Basic_codes/file.java
+++ Basic_codes/file.java
@@ -22,7 +22,7 @@ public class file{
 
 		// System.out.println(((Object)a).getClass().getSimpleName());
 
-		int a=100;
+		int a=1;
 		// this is how we do the typecasting in java
 		char g='a';
 
@@ -31,7 +31,39 @@ public class file{
 
 		char h='a';
 		// double h=998.43;
-		System.out.println(g);
+		System.out.println(a);
+
+		char hh[]={'h','e','l','l'};
+		String aa="hello";
+		String bb="hello";
+		String gg=new String("hello");
+		if(aa==bb)
+			// System.out.println(true);
+		// if(aa==gg)
+		// 	System.out.println(false);
+	
+		
+		// if you are doing aa==bb this will check reference same if you check for the string matching use string function matches 
+
+		// int f=11;
+		// Integer ah=11;
+		// if(f==ah)
+		// 	System.out.println(false);
+		// else 
+		// 	System.out.println(true);
+
+		// the function charAt is used to fetch character at perticular index inside the string 
+		// wrong indexing will lead you to error 
+		System.out.println(bb.charAt(-1));
+
+
+		// the function indexOf this will return index of substring or character from main string
+		// if that string or value is not there inside the string then return -1 
+		System.out.println(bb.indexOf("gg"));
+
+		// substring function is used to create a small string from big string 
+		System.out.println(bb.substring(0,4));
+		
 	}
 
 	//to run the java code we have to first compile it with javac and then we have to run .class file with the keyword java
