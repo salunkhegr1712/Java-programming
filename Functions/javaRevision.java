9d92cca4db2eaf703c94e0c17e5f44ae53cf5691
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-12-20 18:29:04 +0530

    added oops code

--- Functions/javaRevision.java
+++ Functions/javaRevision.java
@@ -3,12 +3,16 @@ import java.lang.*;
 import java.util.*;
 
 // print the prime numbers from a range of the numbers    
-
-
+ 
 // the main class
 public class javaRevision{
+
+    // public class circle{
+    //     double radius;
+    // }
  
-    // in java all the function are written inside the same java class 
+    // in java all the function are written inside the same 
+
     
     // we are going to write the code such that it will return that a given number is prime or not
 
@@ -35,6 +39,8 @@ public class javaRevision{
         }
         return 1;
     }
+    // declaring a global variable 
+    static int abc=10;
 
     static int isPrime2(int num){
         if(num==1 || num <=0){
@@ -71,17 +77,27 @@ public class javaRevision{
         // prime number among list 
         Scanner input=new Scanner(System.in);
 
-        System.out.print("Enter the number : ");
-        int n=input.nextInt();
+        // System.out.print("Enter the number : ");
+        // int n=input.nextInt();
         
-        int k=0;
-        System.out.println("the prime number among 0 to "+n +" are : ");
-        for(int i=1;i<n;i++){
-            if(isPrime2(i)==1){
-                System.out.println(""+i);
-                k++;
-            }
-        }
-        System.out.println("total number of prime number from 1 to "+n+" is : "+k);
+        // int k=0;
+        // System.out.println("the prime number among 0 to "+n +" are : ");
+        // for(int i=1;i<n;i++){
+        //     if(isPrime2(i)==1){
+        //         System.out.println(""+i);
+        //         k++;
+        //     }
+        // }
+        // System.out.println("total number of prime number from 1 to "+n+" is : "+k);
+        
+        // just like the function inside the class we are written the code for the
+        // member of class
+        System.out.println(abc);
+
+        // circle c1=new circle();
+
+        // c1.radius=10;
+    
+
     }
 }
