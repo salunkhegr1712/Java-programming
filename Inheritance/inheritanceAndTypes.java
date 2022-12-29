b06af2d9eb41e8639ce5a62c61d8f439600dc69a
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-12-29 19:36:11 +0530

    added code on type of inheritance

---
+++ Inheritance/inheritanceAndTypes.java
@@ -0,0 +1,32 @@
+// importing basic functionalities from the java library
+import java.lang.*;
+import java.util.*;
+
+// 1) single inheritance
+class Block1{}
+class Block2 extends Block1{}
+
+// 2) Multiple inheritance
+class b1{}
+class b2 extends b1{}
+class b3 extends b2{}
+
+// 3) heiarchical inheritance
+
+class s1{}
+class s2 extends s1{}
+class s3 extends s1{}
+class s4 extends s1{}
+class s5 extends s2{}
+class s6 extends s2{}
+
+// heiarchical also shows Multiple types of inheritance also 
+
+// the main class
+public class inheritanceAndTypes{
+
+    // main function of our java code
+    public static void main(String args[]){
+
+    }
+}
