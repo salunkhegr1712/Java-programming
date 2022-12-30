e19cab4e7016f0df60ddc5e95517b0634853c689
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-12-30 17:17:25 +0530

    added Inner Class codes

--- InnerClasses/AnonymousInnerClass.java
+++ InnerClasses/AnonymousInnerClass.java
@@ -12,11 +12,69 @@ abstract class Joker{
     abstract void function();
 }
 
+// so now we are going to write the code for anonymous inner class
+
+class Outer{
+
+    private int a=10;
+    public int m=1100;
+
+    // declaring anonymous class as nested anonymous class over abstract class
+    // this is one way 
+    Joker z= new Joker(){
+        public void function(){
+            System.out.println("anonymous inner class over abstract class");
+        }
+    };
+
+    // nested anonymous class over a interface 
+    Hell np= new Hell(){
+        public void method1(){
+            System.out.println("anonymous inner class over interface");
+        }
+    };
+
+    // now lets write a function to test nested anonymous inner class
+    public void test(){
+        np.method1();
+        z.function();
+    }
+
+    // also you will able to create a local anonymous class over abstract class or interface
+    // inside a function
+
+    public void ghansham(){
+        
+        Joker zz= new Joker(){
+            public void function(){
+                System.out.println("from function : anonymous inner class over abstract class");
+            }
+        };
+    
+        // nested anonymous class over a interface 
+        Hell nnp= new Hell(){
+            public void method1(){
+                System.out.println("for function : anonymous inner class over interface");
+            }
+        };
+
+        // now lets call the object and test function 
+        zz.function();
+        nnp.method1();
+        }
+    
+}
 // the main class
 public class AnonymousInnerClass{
 
     // main function of our java code
     public static void main(String args[]){
         
+        // now lets run the code 
+        // lets create a object first 
+        Outer a=new Outer();
+        a.test();
+        a.ghansham();
+    
     }
 }
