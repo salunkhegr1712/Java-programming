997b2272ea9b4f4a1602c540db4a21941fd7ea2d
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2023-01-02 15:12:32 +0530

    start studying about packages

---
+++ Packages/packagesInJava.java
@@ -0,0 +1,12 @@
+// import class at start
+import java.lang.String;
+
+public class packagesInJava{
+
+    // main function of our java code
+    public static void main(String args[]){
+
+        // create a String class object 
+        java.lang.String a = new java.lang.String();
+    }   
+}
