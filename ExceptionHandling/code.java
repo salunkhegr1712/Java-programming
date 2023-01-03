14a0b4b484202e1707de1dc79359761f5674d670
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2023-01-03 21:51:33 +0530

    added code and notes on the exception handling

---
+++ ExceptionHandling/code.java
@@ -0,0 +1,17 @@
+// inside this code i will use the code to see and learn the different types of error 
+// which can be tackled while working with the programme
+
+// importing basic functionalities from the java library
+import java.lang.*;
+import java.util.*;
+
+// the main class
+public class code{
+
+    // main function of our java code
+    public static void main(String args[]){
+        // System.out.println("hello world');//unclosed string literal
+        
+    
+    }
+}
