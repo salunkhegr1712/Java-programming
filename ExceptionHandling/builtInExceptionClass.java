6482737c4b798f2ee8250000e6218a379fdde993
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2023-01-04 21:29:13 +0530

    starting with types of exceptions and userdefinedExceptions

---
+++ ExceptionHandling/builtInExceptionClass.java
@@ -0,0 +1,19 @@
+// java has a predefined class which is very largly connected with the exception
+// Exception class
+
+// importing basic functionalities from the java library
+import java.lang.*;
+import java.util.*;
+
+// inside this code i will briefy explain the three important function inside the Exception class
+// a) String getMessage(); String is returntype of the function
+// b) String toString();
+// c) void printStackTree()
+// the main class
+public class builtInExceptionClass {
+
+    // main function of our java code
+    public static void main(String args[]){
+
+    }
+}
---
+++ ExceptionHandling/heiarchyOfExceptions.java
@@ -0,0 +1,1 @@
+// inside this code i will explain about the heiarchy inside the java exceptions 
