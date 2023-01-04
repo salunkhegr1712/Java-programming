5dfb3fad28c685770b6db304c508d352172a7f2f
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2023-01-04 22:36:46 +0530

    completed heiarchy and types of exceptions & code on Exception Class functions

--- ExceptionHandling/builtInExceptionClass.java
+++ ExceptionHandling/builtInExceptionClass.java
@@ -7,13 +7,36 @@ import java.util.*;
 
 // inside this code i will briefy explain the three important function inside the Exception class
 // a) String getMessage(); String is returntype of the function
+
+    // this function will return the String which contains the exception error message from that object 
+
 // b) String toString();
+    // this function is by default function inside the System.out.println("");
+    // and which is able to return String for any object provided 
+
 // c) void printStackTree()
+    // this is function which found sometime usefull as it can be used in order to find out in which order the function
+    // where called and with this info we can be able to solve some problems  
+
 // the main class
 public class builtInExceptionClass {
 
     // main function of our java code
     public static void main(String args[]){
+
+        // so now we are going to see much more information about the built in java class called exception
+
+        Exception m=new Exception();
+        System.out.println(m.getMessage());
+        // it will return null as the exception object does not consist any type of exception
+        // null
+
+        // System.out.println(m.toString());
+        //java.lang.Exception
+
+        m.printStackTrace();
+        // java.lang.Exception
+        // at builtInExceptionClass.main(builtInExceptionClass.java:29)
 
     }
 }
--- ExceptionHandling/heiarchyOfExceptions.java
+++ ExceptionHandling/heiarchyOfExceptions.java
@@ -1,1 +1,79 @@
 // inside this code i will explain about the heiarchy inside the java exceptions 
+// importing basic functionalities from the java library
+import java.lang.*;
+import java.util.*;
+
+// the main class
+public class heiarchyOfExceptions{
+
+    // main function of our java code
+    public static void main(String args[]){
+
+        // the Exception is enough to handle all types of exceptions inside the code 
+        // nested try catch blocks
+        // try{
+        //     int a=10,b=0,c;
+        //     try{
+        //         c=a/b;
+        //     }
+        //     catch(Exception z){
+        //         System.out.println(z);
+        //     }
+        //     int aa[]= new int[5];
+        //     System.out.println("element at position 10 : "+aa[10]);
+        // }
+        // catch(Exception z){
+        //     System.out.println(z);
+            
+        // }
+        // output :
+        // java.lang.ArithmeticException: / by zero
+        // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 5
+
+
+        // try{
+        //     int a=10,b=0,c;
+        //     try{
+        //         c=a/b;
+        //     }
+        //     catch(ArithmeticException z){
+        //         System.out.println(z);
+        //     }
+        //     int aa[]= new int[5];
+        //     System.out.println("element at position 10 : "+aa[10]);
+        // }
+        // catch(ArrayIndexOutOfBoundsException m){
+        //     System.out.println(m);
+        // }
+        // catch(ArithmeticException m){
+        //     System.out.println("");
+        // }
+        // catch(Exception z){
+        //     System.out.println(z);
+            
+        // }
+        // output :
+        // java.lang.ArithmeticException: / by zero
+        // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 5
+        
+        try{
+            int aa[]= new int[5];
+            System.out.println("element at position 10 : "+aa[10]);
+        }
+        
+        catch(Exception z){
+            System.out.println(z);
+            
+        }
+        catch(ArrayIndexOutOfBoundsException m){
+            System.out.println(m);
+        }
+        catch(ArithmeticException m){
+            System.out.println("");
+        }
+        // heiarchyOfExceptions.java:77: error: exception ArithmeticException has already been caught
+        // catch(ArithmeticException m){
+        // 1 error
+        
+    }
+}
